77191d54504d7a382e48883b5244da95b692cfe7
Babydongdong <907322015@qq.com>
2025-10-17 03:53:23 +0800

    feat: pinia

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -4,10 +4,12 @@ import './assets/styles/theme.less'
 import './assets/icons/iconfont.css'
 
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
+app.use(createPinia())
 app.use(router)
 app.mount('#app')
